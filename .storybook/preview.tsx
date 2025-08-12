9936e5ec503ddaa90293a63dc65735520c0b0869
gahyeon <gus_05@naver.com>
2025-08-13 01:51:30 +0900

    chore: 스토리북 preview에 global styles, decorators  설정 추가 (#13)

---
+++ .storybook/preview.tsx
@@ -0,0 +1,35 @@
+import type { Preview } from '@storybook/react-vite'
+import { ThemeProvider } from 'styled-components'
+import { MemoryRouter } from 'react-router-dom'
+import { theme } from '../src/shared/styles/theme'
+import GlobalStyle from '@/shared/styles/globalStyle'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo',
+    },
+    layout: 'centered',
+  },
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Story />
+        </ThemeProvider>
+      </MemoryRouter>
+    ),
+  ],
+}
+
+export default preview
