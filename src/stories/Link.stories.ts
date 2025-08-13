562f11976d30f062ff79d750e1ae75ecba3ddee6
gahyeon <gus_05@naver.com>
2025-08-14 02:34:24 +0900

    feat: link 컴포넌트 스토리 파일 작성 (#16)

---
+++ src/stories/Link.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import Link from '@/shared/ui/Link'
+
+const meta: Meta<typeof Link> = {
+  title: 'Shared/Link',
+  component: Link,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: ['large', 'small'],
+    },
+    data: {
+      control: 'object', // 사용자 입력 가능
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof Link>
+
+const defaultData = {
+  thumbnail: null,
+  title:
+    '유튜브 링크 제목이 들어갑니다. 2줄 예시입니다. 최대 2줄까지 들어가며, 2줄을 넘을 경우 말줄임 처리됩니다.',
+}
+
+export const Interactive: Story = {
+  args: {
+    variant: 'large',
+    data: defaultData,
+  },
+}
