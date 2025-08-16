8b735729d21af6f255c6d204b348e23c75ecf1e8
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2025-08-16 21:44:04 +0900

    feat: 전체 레이아웃에 NavBar 추가 및 nav 영역 background 설정 (#19)

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,26 @@ import styled from 'styled-components'
 
 import { AppRoutes } from '@app/routes/routes'
 
-import { useDevice, type DeviceType } from '@/shared/hooks/useDevice'
+import { useDevice, type DeviceType } from '@shared/hooks/useDevice'
+import { flexRowCenter } from '@shared/styles/mixins'
+import NavBar, { NAV_HEIGHT } from '@shared/ui/NavBar'
 
 const App = () => {
   const deviceType = useDevice()
 
+  const LAYOUT_WIDTH = deviceType === 'mobile' ? 'clamp(320px, 100dvw, 430px)' : '375px'
+  const LAYOUT_BOTTOM_GAP = 34
+
   return (
-    <MainLayout $deviceType={deviceType}>
+    <MainLayout
+      $deviceType={deviceType}
+      $layoutWidth={LAYOUT_WIDTH}
+      $layoutBottomGap={LAYOUT_BOTTOM_GAP}
+    >
       <AppRoutes />
+      <NavContainer $layoutWidth={LAYOUT_WIDTH} $layoutBottomGap={LAYOUT_BOTTOM_GAP}>
+        <NavBar />
+      </NavContainer>
     </MainLayout>
   )
 }
@@ -18,15 +30,32 @@ export default App
 
 const MainLayout = styled.main<{
   $deviceType: DeviceType
+  $layoutWidth: string
+  $layoutBottomGap: number
 }>`
   position: relative;
   margin: 0 auto;
-  padding: 0 20px;
-  width: ${({ $deviceType }) =>
-    $deviceType === 'mobile' ? 'clamp(320px, 100dvw, 430px)' : '375px'};
+  padding: 0 20px ${({ $layoutBottomGap }) => NAV_HEIGHT + $layoutBottomGap}px 20px;
+  width: ${({ $layoutWidth }) => $layoutWidth};
   min-height: 100dvh;
   overflow-x: auto;
   overflow-y: auto;
-  background-color: #0f1013;
+  background-color: ${({ theme }) => theme.COLOR['gray-900']};
   color: #fafbfe;
+`
+
+const NavContainer = styled.div<{
+  $layoutWidth: string
+  $layoutBottomGap: number
+}>`
+  z-index: 100;
+  position: fixed;
+  left: 50%;
+  bottom: 0;
+  margin: 0 -20px 0 0px;
+  ${flexRowCenter}
+  width: ${({ $layoutWidth }) => $layoutWidth};
+  padding-bottom: ${({ $layoutBottomGap }) => $layoutBottomGap}px;
+  transform: translateX(-50%);
+  background: linear-gradient(to bottom, rgba(15, 16, 20, 0), rgba(15, 16, 20, 1), rgb(15, 16, 20));
 `
