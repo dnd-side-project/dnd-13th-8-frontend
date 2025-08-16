694cb5721a5c51a51a5e76002487b3f24bec950d
gahyeon <gus_05@naver.com>
2025-08-17 05:52:05 +0900

    feat: 청취자 수를 나타내는 LiveInfo 컴포넌트 구현 (#45)

---
+++ src/shared/ui/LiveInfo.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+import { User } from '@/assets/icons'
+
+interface LiveInfoProps {
+  isOnAir: boolean
+  listenerCount: number
+}
+
+const LiveInfo = ({ isOnAir, listenerCount }: LiveInfoProps) => {
+  return (
+    <Wrapper>
+      {isOnAir && <OnAirBadge>On Air</OnAirBadge>}
+      <ListenerCount>
+        <User /> {listenerCount}
+      </ListenerCount>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  ${({ theme }) => theme.FONT.caption1};
+`
+
+const OnAirBadge = styled.span`
+  background-color: ${({ theme }) => theme.COLOR['gray-600']};
+  color: ${({ theme }) => theme.COLOR['primary-normal']};
+  font-weight: 600;
+  padding: 6px 11px;
+  border-radius: 6px;
+`
+
+const ListenerCount = styled.span`
+  display: flex;
+  gap: 2px;
+  color: ${({ theme }) => theme.COLOR['gray-10']};
+`
+
+export default LiveInfo
