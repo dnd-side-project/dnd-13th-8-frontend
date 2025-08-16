a3709eefe4d76b5b537d22bf3d1cf0f28ad6f656
gahyeon <gus_05@naver.com>
2025-08-17 03:01:09 +0900

    feat: 검색 페이지 구현 (#37)

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -7,12 +7,14 @@ import PrivateRoute from '@widgets/authGuard/PrivateRoute'
 
 const MyPage = lazy(() => import('@/pages/myPage/ui'))
 const HomePage = lazy(() => import('@/pages/homePage'))
+const SearchPage = lazy(() => import('@/pages/searchPage'))
 
 export const AppRoutes = () => {
   return (
     <Suspense fallback={<Loading isLoading width="100%" height="100%" />}>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
         <Route
           path="/mypage/*"
           element={
