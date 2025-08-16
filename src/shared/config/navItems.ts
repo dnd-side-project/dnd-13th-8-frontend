dcfc068ebeced897a86154ecc1bb5d0aed759a3f
gahyeon <gus_05@naver.com>
2025-08-17 03:11:10 +0900

    fix: 네비게이션 바 여러 경로 지원하도록 수정 (#37)

--- src/shared/config/navItems.ts
+++ src/shared/config/navItems.ts
@@ -5,12 +5,12 @@ import { Home, Look, Music, Person } from '@/assets/icons'
 interface NavItem {
   icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
   title: string
-  path: string
+  paths: string[] // 여러 경로 허용
 }
 
 export const NAV_ITEMS: NavItem[] = [
-  { icon: Home, title: '홈', path: '/' },
-  { icon: Look, title: '둘러보기', path: '/discover' },
-  { icon: Music, title: '나의 CD', path: '/mycd' },
-  { icon: Person, title: '마이페이지', path: '/mypage' },
+  { icon: Home, title: '홈', paths: ['/', '/search', '/searchResult'] },
+  { icon: Look, title: '둘러보기', paths: ['/discover'] },
+  { icon: Music, title: '나의 CD', paths: ['/mycd'] },
+  { icon: Person, title: '마이페이지', paths: ['/mypage'] },
 ]
