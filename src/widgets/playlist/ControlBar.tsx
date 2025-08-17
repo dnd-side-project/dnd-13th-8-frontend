4d6ffd5b9e403c91ff234122b60ceaeda4cdca67
gahyeon <gus_05@naver.com>
2025-08-17 09:18:18 +0900

    feat: 음악 재생용 ControlBar 컴포넌트 생성 (이전/재생/다음 버튼) (#45)

---
+++ src/widgets/playlist/ControlBar.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+import { Next, Prev, Start } from '@/assets/icons'
+import { SvgButton } from '@/shared/ui'
+
+const ControlBar = () => {
+  const handlePrev = () => {
+    // TODO: 이전 플레이리스트로 이동하는 로직 작성
+  }
+
+  const handleStart = () => {
+    // TODO: 재생/일시정지 로직 작성
+  }
+
+  const handleNext = () => {
+    // TODO: 다음 플레이리스트로 이동하는 로직 작성
+  }
+
+  return (
+    <Wrapper>
+      <SvgButton icon={Prev} width={32} height={32} onClick={handlePrev} />
+      <SvgButton icon={Start} width={32} height={32} onClick={handleStart} fill="#ffffff" />
+      <SvgButton icon={Next} width={32} height={32} onClick={handleNext} />
+    </Wrapper>
+  )
+}
+
+export default ControlBar
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 32px 60px 24px 60px;
+`
