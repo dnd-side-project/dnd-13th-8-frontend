92e992103691042c06a5f6296cbc0e91c1e5fdf5
gahyeon <gus_05@naver.com>
2025-08-17 18:00:08 +0900

    fix: transient prop()로 수정 (#37)

--- src/shared/ui/CategoryButton.tsx
+++ src/shared/ui/CategoryButton.tsx
@@ -6,7 +6,7 @@ interface CategoryButtonProps {
 }
 
 const CategoryButton = ({ text, size }: CategoryButtonProps) => {
-  return <StyledButton size={size}>{text}</StyledButton>
+  return <StyledButton $size={size}>{text}</StyledButton>
 }
 
 export default CategoryButton
@@ -16,12 +16,12 @@ const sizes = {
   large: { width: '160px', height: '200px', borderRadius: '10px', padding: '12px' },
 }
 
-const StyledButton = styled.button<{ size: 'small' | 'large' }>`
+const StyledButton = styled.button<{ $size: 'small' | 'large' }>`
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
   background-color: ${({ theme }) => theme.COLOR['gray-700']};
   color: ${({ theme }) => theme.COLOR['gray-10']};
 
-  ${({ size }) => css(sizes[size])}
+  ${({ $size }) => css(sizes[$size])}
 `
