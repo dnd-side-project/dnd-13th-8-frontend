961b659fc73323de6965e02ad44982f40fa78bf1
gahyeon <gus_05@naver.com>
2025-10-23 13:06:05 +0900

    fix: Playlist 컴포넌트 스토리북 오류 수정 (#113)

--- src/stories/Playlist.stories.tsx
+++ src/stories/Playlist.stories.tsx
@@ -1,11 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import Playlist from '@/widgets/playlist/Playlist'
+
+const queryClient = new QueryClient()
 
 const meta: Meta<typeof Playlist> = {
   title: 'Widgets/Playlist/Playlist',
   component: Playlist,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
 }
 
 export default meta
@@ -14,13 +24,6 @@ type Story = StoryObj<typeof Playlist>
 export const Default: Story = {
   args: {
     title: '작업할 때 듣기 좋은 플레이리스트. 길어지면 말줄임표가 나온답니다',
-    username: 'Team 8樂8樂',
-  },
-}
-
-export const Liked: Story = {
-  args: {
-    title: '이미 좋아요 된 플레이리스트 예시',
     username: 'Team 8樂8樂',
   },
 }
