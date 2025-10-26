7642f43acc40b60ed22e1bf43f38de696f9c6df0
gahyeon <gus_05@naver.com>
2025-10-26 17:04:40 +0900

    fix: my 탭 isPublic true 필터링 로직 삭제 (#123)

--- src/pages/mycd/index.tsx
+++ src/pages/mycd/index.tsx
@@ -47,11 +47,7 @@ const MyCdPage = () => {
 
   const playlistQuery = selectedTab === 'MY' ? myCdPlaylist : likedCdPlaylist
 
-  const playlistData = useMemo(
-    () => playlistQuery.data?.filter((p) => selectedTab !== 'MY' || p.isPublic), // my 탭일 경우 isPublic true만 필터링
-
-    [playlistQuery.data, selectedTab]
-  )
+  const playlistData = useMemo(() => playlistQuery.data ?? [], [playlistQuery.data])
 
   const isError = playlistQuery.isError
 
