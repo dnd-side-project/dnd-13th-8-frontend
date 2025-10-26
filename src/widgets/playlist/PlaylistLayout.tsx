176114c9e61d4dd6c7f3f4f66de46acdae8c1aeb
gahyeon <gus_05@naver.com>
2025-10-27 08:13:58 +0900

    fix: playlist layout cd 타입 수정 (#122)

--- src/widgets/playlist/PlaylistLayout.tsx
+++ src/widgets/playlist/PlaylistLayout.tsx
@@ -71,18 +71,14 @@ const PlaylistLayout = ({
         <CdContainer>
           {!isPlaying && <PlayButton onPlayPause={onPlayPause} />}
           <CdSpinner $isPlaying={isPlaying}>
-            <Cd
-              variant="xxl"
-              bgColor="none"
-              stickers={data?.cdItems ?? data?.onlyCdResponse?.cdItems ?? []}
-            />
+            <Cd variant="xxl" bgColor="none" stickers={data?.cdItems ?? data?.cdItems ?? []} />
           </CdSpinner>
         </CdContainer>
         <ActionBarContainer>
           <ActionBar
             playlistId={data.playlistId}
             creatorId={data.creator.creatorId}
-            stickers={data?.cdItems ?? data?.onlyCdResponse?.cdItems ?? []}
+            stickers={data?.cdItems ?? data?.cdItems ?? []}
             type="DISCOVER"
           />
         </ActionBarContainer>
