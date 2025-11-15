2c6a951b9f47fd9542a1401d4e8f93b5724ded15
gahyeon <gus_05@naver.com>
2025-11-16 02:44:52 +0900

    fix: LiveInfo 컴포넌트 스토리북 파일 수정 (#156)

--- src/stories/LiveInfo.stories.tsx
+++ src/stories/LiveInfo.stories.tsx
@@ -15,28 +15,15 @@ const meta: Meta<typeof LiveInfo> = {
       control: 'number',
       description: '실시간 청취자 수',
     },
-    isOwner: {
-      control: 'boolean',
-      description: '플레이리스트 소유자 여부 (ON AIR 버튼을 클릭하여 on/off 가능)',
-    },
   },
 }
 
 export default meta
 type Story = StoryObj<typeof LiveInfo>
 
-export const Owner: Story = {
+export const Default: Story = {
   args: {
     isOnAir: true,
     listenerCount: 550,
-    isOwner: true,
-  },
-}
-
-export const NotOwner: Story = {
-  args: {
-    isOnAir: true,
-    listenerCount: 550,
-    isOwner: false,
   },
 }
